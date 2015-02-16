edebc007c1d59545251ebeec7b042d0437a86707
Arbab Nazar <arbabnazar@users.noreply.github.com>
2015-02-16 21:52:15 +0500

    Update ipsetting.sh
    Modify the settings

--- ipsetting.sh
+++ ipsetting.sh
@@ -1,26 +1,39 @@
-!/bin/bash
+#!/bin/bash
 
-if [ $# -eq 5 ] then
+if [ $# -eq 5 ]
+then
 
-echo "" echo "Taking the backup and Changing the hostname from $(hostname) to $1 ..."
+echo ""
+echo "Taking the backup and Changing the hostname from $(hostname) to $1 ..."
 
 sed -i.bk "s/$(hostname)/$1/g" /etc/sysconfig/network
 
-echo "" echo "Backing up & Assigning the Static IP ..." echo ""
+echo ""
+echo "Backing up & Assigning the Static IP ..."
+echo ""
 
 cp /etc/sysconfig/network-scripts/ifcfg-$2 /etc/sysconfig/network-scripts/$2.bk
 
-cat < /etc/sysconfig/network-scripts/ifcfg-$2
+cat <<EOF > /etc/sysconfig/network-scripts/ifcfg-$2
 
-DEVICE=$2 BOOTPROTO=static IPADDR=$3.$4 NETMASK=255.255.255.0 GATEWAY=$3.$5 ONBOOT=yes EOF
+DEVICE=$2
+BOOTPROTO=static
+IPADDR=$3.$4
+NETMASK=255.255.255.0
+GATEWAY=$3.$5
+ONBOOT=yes
+EOF
 
-echo "Changing the dns ..." echo ""
+echo "Changing the dns ..."
+echo ""
 
 sed -i.bk "s/nameserver.*/nameserver $3.$5/" /etc/resolv.conf
 
-echo "Adding $1 as hostname to the /etc/hosts file .." echo ""
+echo "Adding $1 as hostname to the /etc/hosts file .."
+echo ""
 
-sed -i.bk "/$(hostname)$/d" /etc/hosts echo "$3.$4 $1" >> /etc/hosts
+sed -i.bk "/$(hostname)$/d" /etc/hosts
+echo "$3.$4 $1" >> /etc/hosts
 
 echo "Restarting the Network Service, Please connect it using the new IP Address if you are using ssh ..."
 
@@ -28,6 +41,7 @@ service network restart
 
 else
 
-echo "Usage: ip.sh " echo "Example: ip.sh testname eth0 10.10.10 41 1"
+echo "Usage: ip.sh <hostname> <interface> <baseip> <ipaddress> <gateway/dns>"
+echo "Example: ip.sh testname eth0 10.10.10 41 1"
 
 fi
